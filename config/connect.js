17c36c534203326ee6d5e1dc37d29498c58f6fcd
Pavel Zakharov <trade93@gmail.com>
2023-05-07 17:17:02 -0500

    fix: mongodb connection

--- config/connect.js
+++ config/connect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connect = async () => {
   const isProduction = process.env.NODE_ENV === 'production';
-  const databaseUrl = isProduction ? process.env.DATABASE_URL : 'mongodb://localhost:27017/bitfilmsdb';
+  const databaseUrl = isProduction ? process.env.DATABASE_URL : 'mongodb://127.0.0.1:27017/bitfilmsdb';
 
   try {
     await mongoose.connect(databaseUrl, {
